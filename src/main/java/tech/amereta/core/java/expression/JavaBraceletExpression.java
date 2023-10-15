386257d20105c4fc6da8ab7a647d1413f1a06e2a
Soroush <shuoros@yahoo.com>
2023-10-15 09:20:23 +0200

    FIX[JavaBraceletExpression.builder was not static]

--- src/main/java/tech/amereta/core/java/expression/JavaBraceletExpression.java
+++ src/main/java/tech/amereta/core/java/expression/JavaBraceletExpression.java
@@ -11,7 +11,7 @@ public class JavaBraceletExpression extends Operable implements Expression {
     private List<Expression> expressions = new LinkedList<>();
     private boolean bracelet = true;
 
-    public JavaBraceletExpression builder() {
+    public static JavaBraceletExpression builder() {
         return new JavaBraceletExpression();
     }
 
